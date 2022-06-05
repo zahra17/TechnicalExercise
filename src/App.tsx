70fc7aed4d3452244d68eab98e1bb3a7643d6126
zahra.farzan <zahra.farzan@zoodfood.com>
2022-06-06 02:12:31 +0430

    fix issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./page/Home/Home";
 import Search from "./page/Search";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+
 const App: React.FC = ({}) => {
   return (
     <div className="App">
